c78e9c0797ad67f58026635990982bb5ce607f9d
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-11 18:42:36 -0700

    in movie.js: pull the certain data from url, & console.log it

--- movie.js
+++ movie.js
@@ -3,14 +3,24 @@ const axios = require("axios");
 // axios geting data part
 const api_key = "trilogy";
 // hard coded movie
-const search = "Sweet November";
+const movie = "Sweet November";
 // main url
-const url = "http://www.omdbapi.com/?t=" + search + "&apikey=" + api_key;
-// geting data
+const url = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + api_key;
+// geting data from url
 axios.get(url).then(function (response) {
-  console.log(response.data);
-  console.log(response.data.Title);
-  console.log(response.data.Year);
+  // console.log(response.data);
+  console.log("Image: " + response.data.Poster);
+  console.log("Movie Name: " + response.data.Title);
+  console.log("Genre: " + response.data.Genre);
+  console.log("Short-Description: " + response.data.Plot);
+  console.log("Actors: " + response.data.Actors);
+  console.log("Director: " + response.data.Director);
+  console.log("Ratings: " + response.data.Ratings[0].Value);
+  console.log("Awards: " + response.data.Awards);
+  console.log("Box-Office: " + response.data.BoxOffice);
+  console.log("Production: " + response.data.Production);
+  console.log("Year: " + response.data.Year);
+
 });
 
 
