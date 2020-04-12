045106e7216d87090136d7d6b4a0a91c2ddc71f2
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-12 00:24:10 -0700

    created READ.md file for basic instructionsabout app, .gitignore: ignored key.js because it contains personal id & key, liri.js: using for loop through the data get specific data from url

---
+++ READ.md
@@ -0,0 +1,11 @@
+*movie.js: for movie search from www.omdbapi.com 
+// hard-coded movie: "Sweet November" ,
+*liri.js: search songs from Sporify.com 
+// hard-coded song: spotify-this-song "I Want it That Way",
+*bands.js: Search Singers/Bands for their up-coming perfomance schedule & location 
+// hard-coded song: "Celine Dion"
+
+
+
+
+Notice: Keys.js file gitignored locally. So you need your spotify user_id & secret for run this app.
--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
-
+// reuire the npm package
 var Spotify = require('node-spotify-api');
 // const axios = require("axios");
-
+// import the key.js
 var keys = require("./keys.js");
+// 
 require("dotenv").config();
 
 // exicuting spotify userID & spotify secret
@@ -16,22 +17,35 @@ var spotify = new Spotify({
 // console.log(keys.spotify.id);
 // console.log(keys.spotify.secret);
 
-const song = "I want it that away";
-
-spotify.search({ type: 'track', query: song }, function (err, data) {
+const song = "west life";
+/* Currently there are two methods available, SEARCH & REQUEST */
+spotify.search({
+  type: 'track',
+  query: song
+}, function (err, data) {
   if (err) {
-    return console.log('Error occurred: ' + err);
+    return console.log('Here Error Massag from consle.log: ' + err);
   }
 
-  // console.log(data.tracks.items[0].album);
   // console.log("Song Name: " + data.tracks.items[0].name);
   // console.log("Artist name: " + data.tracks.items[0].album.artists[0].name); //array
+  // console.log("IMG URL: " + data.tracks.items[0].album.images);
   // console.log("The song link: " + data.tracks.items[0].external_urls.spotify); //obj
   // console.log("Album Name: " + data.tracks.items[0].album.name);
   // console.log("Released Date: " + data.tracks.items[0].album.release_date);
 
+  for (i = 0; i < data.tracks.items.length; i++) {
+    console.log("...................................................");
+    console.log("Song Name: " + data.tracks.items[i].name);
+    console.log("Artist/Band Name name: " + data.tracks.items[i].album.artists[0].name);
+    console.log("IMG URL: " + data.tracks.items[i].album.images);
+    console.log("The song link: " + data.tracks.items[i].external_urls.spotify);
+    console.log("Album Name: " + data.tracks.items[i].album.name);
+    console.log("Released Date: " + data.tracks.items[i].album.release_date);
 
-  console.log("Artist's Image link " + data.tracks.items[0].images); //array?
+  }
+
+
 });
 
 
