e7ec4e7d70d11ff46d1570c8a8909efcdf5d4732
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-11 22:46:40 -0700

    in liri.js: Currently there are two methods available, search and request, But using SEARCH method, get the data & consle.logged certain itmes

--- keys.js
+++ keys.js
@@ -1,6 +1,27 @@
 console.log('massage: key.js page connected');
 
+// exports.spotify = {
+//   id:process.env.SPOTIFY_ID,
+//   secret:process.env.SPOTIFY_SECRET
+// };
+
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id:"248b529dd8f440368719909ef0a3edb6",
+  secret:"477871698bc842aa84162117b79b9aad"
 };
+
+// const URL = "https://api.spotify.com/v1/" + artists + "&api_id="+keys.spotify.id+"&tracks=10";
+
+// const URL = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks";
+
+// const artists = "Celion Dion"
+// const url = "https://api.spotify.com/t=?" + artists +"&api_id=" + keys.spotify.id + "&albums";
+
+/* exapmles of different forms of URL-SET-UP */
+
+// let search = 'lion';
+// const url1 = 'http://www.omdbapi.com/' + '?apikey=trilogy' + '&' + 't=lion&s=&total=';
+// const url2 = 'http://www.omdbapi.com/' +    '?' + "t=${search}"    + '&' + 'apikey=trilogy';
+// const url3 = 'http://www.omdbapi.com/' +    '?' + 't=search'       + '&' + 'apikey=trilogy';
+// const url4 = 'http://www.omdbapi.com/' +    '?' + 't=' + search    + '&' + 'apikey=trilogy';
+// const url5 = 'http://www.omdbapi.com/' +    '?' + `t=${search}`    + '&' + 'apikey=trilogy'; */
--- liri.js
+++ liri.js
@@ -1,69 +1,38 @@
-/* exapmles of different forms of URL-SET-UP */
 
-// let search = 'lion';
-// const url1 = 'http://www.omdbapi.com/' + '?apikey=trilogy' + '&' + 't=lion&s=&total=';
-// const url2 = 'http://www.omdbapi.com/' +    '?' + "t=${search}"    + '&' + 'apikey=trilogy';
-// const url3 = 'http://www.omdbapi.com/' +    '?' + 't=search'       + '&' + 'apikey=trilogy';
-// const url4 = 'http://www.omdbapi.com/' +    '?' + 't=' + search    + '&' + 'apikey=trilogy';
-// const url5 = 'http://www.omdbapi.com/' +    '?' + `t=${search}`    + '&' + 'apikey=trilogy';
-
-require("dotenv").config();
-// reuired from npm
 var Spotify = require('node-spotify-api');
-const axios = require("axios");
+// const axios = require("axios");
 
-// imported from file
 var keys = require("./keys.js");
+require("dotenv").config();
 
 // exicuting spotify userID & spotify secret
-var spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
+// console.log(keys.spotify.id);
+
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
 // console.log(keys.spotify.id);
 // console.log(keys.spotify.secret);
 
-// const artists = "Celion Dion"
-// const url = "https://api.spotify.com/t=?" + artists +"&api_id=" + keys.spotify.id + "&albums";
+const song = "I want it that away";
 
-// // const URL = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks";
+spotify.search({ type: 'track', query: song }, function (err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
 
-// const URL = "https://api.spotify.com/v1/" + artists + "&api_id="+keys.spotify.id+"&tracks=10";
+  // console.log(data.tracks.items[0].album);
+  // console.log("Song Name: " + data.tracks.items[0].name);
+  // console.log("Artist name: " + data.tracks.items[0].album.artists[0].name); //array
+  // console.log("The song link: " + data.tracks.items[0].external_urls.spotify); //obj
+  // console.log("Album Name: " + data.tracks.items[0].album.name);
+  // console.log("Released Date: " + data.tracks.items[0].album.release_date);
 
-const userID = keys.spotify.id;
-const token = keys.spotify.secret;
 
-const url = "https://api.spotify.com/v1/users/" + userID + "/playlists";
-
-axios.get({
-  url:url,
-  auth: token
-}).then(function(response){
-  console.log(response);
-  
-})
-
-/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
-
-// let songName = "";
-
-// for (var i = 2; i < process.argv.length; i++) {
-
-//   if (i != process.argv.length - 1) {
-//     songName += process.argv[i] + " ";
-//   } else {
-//     songName += process.argv[i];
-//   }
-
-// }
-// // console.log(songName);
-
-// spotify.search({ type: 'track', query: songName }, function (err, data) {
-
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-
-//   // console.log(data);
-//   // console.log(data.tracks.items[0].artists[0].name);
-// });
+  console.log("Artist's Image link " + data.tracks.items[0].images); //array?
+});
 
 
 
