3a6bc24e622f1411b2dd6b944036252f941ace05
Jatinokov <Enkhtaivan1206@gmail.com>
2020-05-13 18:08:48 -0700

    conditional statement for array object

--- client/app.js
+++ client/app.js
@@ -18,104 +18,119 @@ $(document).ready(function () {
         axios.get(`http://localhost:5000/api/${inputValue}`).then(function (response) {
             console.log(response.data);
             // console.log(response.data[0].datetime);
-            console.log(response.data[0].venue.country);
+            // console.log(response.data[0].venue.country);
+
             // creating divs here
             var display = $("<div class='display'/>");
             $(".basic").append(display);
-            var items = $("<div class='ui items'/>");
-            $(".display").append(items);
-            var item = $("<div class='item' />");
-            $(".items").append(item);
-            var imageDiv = $("<div class='imageDiv'/>");
-            $(".item").append(imageDiv);
-            var today = $("<div class='today'/>");
-            $(".imageDiv").append(today);
-
-            var content = $("<div class='content'/>");
-            $(".item").append(content);
-            var band = $("<div class='band'/><br/>");
-            $(".content").append(band);
-            var ShowTime = $("<div class='ShowTime'/><br/>");
-            $(".content").append(ShowTime);
-
-            var palace = $("<div class='palace'/>");
-            $(".content").append(palace);
-            var city = $("<div class='city'/>");
-            $(".content").append(city);
-            var country = $("<div class='country'/><br/>");
-            $(".content").append(country);
-            var button = $("<div class='button'/>");
-            $(".content").append(button);
 
 
+            // if (response.data[i].hasOwnProperty("artist")) {
+
+            // var image = $("<img height='200px' width='200px'/>");
+            // image.addClass("ui imz")
+            // image.attr("src", response.data[i].artist.thumb_url);
+            // $(".imageDiv").append(image);
+            // $(".imz").append(response.data[i].artist.thumb_url);
+
+            // }
 
 
 
             // creating tags here
 
-            // for (let i = 0; i < response.data.length; i++) {
+            for (let i = 0; i < response.data.length; i++) {
+                // console.log(response.data.length);
+                if (response.data[i] == response.data.length) {
 
-            //     var image = $("<img height='200px' width='200px'/>");
-            //     image.addClass("ui imz")
-            //     image.attr("src", response.data[i].artist.thumb_url);
-            //     $(".imageDiv").append(image);
-            //     $(".imz").append(response.data[i].artist.thumb_url);
+                    var items = $("<div class='ui items'/>");
+                    $(".display").append(items);
 
-            //     var bandName = $("<span class='ui bandName'/>");
-            //     bandName.text("Band Name:  ");
-            //     $(".band").append(bandName);
-            //     $(".bandName").append("<b><a href='#'>" + response.data[i].lineup + "</a></b>");
+                    var item = $("<div class='item' />");
+                    $(".items").append(item);
 
-            //     var concertDate = $("<span class='ui ShowDate'/>");
-            //     concertDate.text("Show Date: ");
-            //     $(".ShowTime").append(concertDate);
-            //     $(".ShowDate").append("<b><a href='#'>" + response.data[i].datetime + "</a></b>");
+                    var imageDiv = $("<div class='imageDiv'/>");
+                    $(".item").append(imageDiv);
 
-            //     var palaza = $("<p class='plaza'/>");
-            //     palaza.text("Palace:  ");
-            //     $(".palace").append(palaza);
-            //     $(".plaza").append("<b><a href='#'>" + response.data[i].venue.name + "</a></b>");
+                    var today = $("<div class='today'/>");
+                    $(".imageDiv").append(today);
 
-            //     var city = $("<p class='ShowCity'/>");
-            //     city.text("City:  ");
-            //     $(".city").append(city);
-            //     $(".ShowCity").append("<b><a href='#'>" + response.data[i].venue.city + "</a></b>");
+                    var content = $("<div class='content'/>");
+                    $(".item").append(content);
 
-            //     var country = $("<p class='ShowCountry'/>");
-            //     country.text("Country:  ");
-            //     $(".country").append(country);
-            //     $(".ShowCountry").append("<b><a href='#'>" + response.data[i].venue.country + "</a></b>");
+                    var band = $("<div class='band'/><br/>");
+                    $(".content").append(band);
 
-            //     var buttonOne = $("<button class='ui btn1'/>");
-            //     buttonOne.text("save");
-            //     $(".button").append(buttonOne);
-            //     // $("btn").append(action will be here)
+                    var ShowTime = $("<div class='ShowTime'/><br/>");
+                    $(".content").append(ShowTime);
 
-            //     var buttonTwo = $("<button class='ui btn2'/>");
-            //     buttonTwo.text("delete");
-            //     $(".button").append(buttonTwo);
-            //     // $("btn").append(action will be here)
+                    var palace = $("<div class='palace'/>");
+                    $(".content").append(palace);
 
-            //     var todayDate = $("<span class='ui todayDate'/>");
-            //     todayDate.text("Today : ");
-            //     $(".today").append(todayDate);
-            //     $(".todayDate").append("<b>" + formated + "</b>");
+                    var city = $("<div class='city'/>");
+                    $(".content").append(city);
+
+                    var country = $("<div class='country'/><br/>");
+                    $(".content").append(country);
+
+                    var button = $("<div class='button'/>");
+                    $(".content").append(button);
 
 
-            // }
 
-            let i;
-            for(i=0; i< response.data.length; i++){
 
-               console.log(response.data[i].venue.name, i);
-              
-               
-            } 
+                    var bandName = $("<span class='ui bandName'/>");
+                    // bandName.text("Band Name:  ");
+                    $(".band").append(bandName);
+                    $(".bandName").append("<b><a href='#'>" + " Band Name: " + response.data[i].lineup + "</a></b>");
 
-              
+                    var concertDate = $("<span class='ui ShowDate'/>");
+                    // concertDate.text("Show Date: ");
+                    $(".ShowTime").append(concertDate);
+                    $(".ShowDate").append("<b><a href='#'>" + "Show Date: " + response.data[i].datetime + "</a></b>");
+
+                    var palaza = $("<p class='plaza'/>");
+                    // palaza.text("Palace:  ");
+                    $(".palace").append(palaza);
+                    $(".plaza").append("<b><a href='#'>" + "Palace: " + response.data[i].venue.name + "</a></b>");
+
+                    var city = $("<p class='ShowCity'/>");
+                    // city.text("City:  ");
+                    $(".city").append(city);
+                    $(".ShowCity").append("<b><a href='#'>" + "City: " + response.data[i].venue.city + "</a></>");
+
+                    var country = $("<p class='ShowCountry'/>");
+                    // country.text("Country:  ");
+                    $(".country").append(country);
+                    $(".ShowCountry").append("<b><a href='#'>" + "Country: " + response.data[i].venue.country + "</a></b>");
+
+                    var buttonOne = $("<button class='ui btn1'/>");
+                    buttonOne.text("save");
+                    $(".button").append(buttonOne);
+                    // $("btn").append(action will be here)
+
+                    var buttonTwo = $("<button class='ui btn2'/>");
+                    buttonTwo.text("delete");
+                    $(".button").append(buttonTwo);
+                    // $("btn").append(action will be here)
+
+                    // var todayDate = $("<span class='ui todayDate'/>");
+                    // todayDate.text("Today : ");
+                    // $(".today").append(todayDate);
+                    // $(".todayDate").append("<b>" + formated + "</b>");
+
+                }else{
+                    console.log("reached length of the array");
+                    
+                }
+            }
+
 
         });
-        //     for (let i = 0; i < response.data.length; i++) {
+    });
+});
+
+    //     for (let i = 0; i < response.data.length; i++) {
 
         //         $(".img").append(response.data[i].artist.thumb_url);
         //         $(".display").append(response.data[i].artist.name);
@@ -136,5 +151,3 @@ $(document).ready(function () {
         //     }
         // }).catch(err => console.log(err));
         // });
-    });
-})
